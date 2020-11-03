7255145a7b1d75ead0772fa8c0bb7d79721d1584
ph-u <51484056+ph-u@users.noreply.github.com>
2020-11-03 11:25:01 +0000

    build.sh -> automated pipeline set-up

---
+++ build.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# author: ph-u
+# script: build.sh
+# desc: Build agalony pipeline
+# in: bash build.sh
+# out: set-up agalony
+# arg: 0
+# date: 20201103
+
+##### orientate #####
+mAs=`dirname $0` && cd ${mAs}
+mAs=`echo -e "${mAs}/code/"`
+
+##### process #####
+[[ `which python|wc -l` -lt 1 ]] && [[ `which R|wc -l` -lt 1 ]] && echo -e "need python3 or R" ## python3 / R exist?
+
+if [[ `which python|wc -l` -ge 1 ]];then
+	pip3 2> tEst.txt 1> tEst.txt ## pip3 exist?
+	if [[ `wc -l < tEst.txt` -gt 1 ]]
+	then pip3 install -r requirements.txt ## install python3 packages
+	else echo -e "need pip package installer for python3"
+	fi
+	rm tEst.txt
+fi
+
+if [[ `which R|wc -l` -ge 1 ]];then
+	Rscript -e "if(suppressMessages(suppressWarnings(require(imager)))==F){install.packages('imager', dependencies=T, repos='https://cloud.r-project.org/')}"
+fi
+
+cd ${mAs} && gcc csv2data.c -o c2d
+
+echo -e "agalony successfully-built\n\n       Thank you for taking a step towards reproducible projects\n\n"
+exit 0
+
